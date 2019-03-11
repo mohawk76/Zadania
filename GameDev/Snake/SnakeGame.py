9647e7e05b2f5c29610daf2a4cd432a61c69ed0f
local <mohawk76@wp.pl>
2019-03-11 10:14:01 +0100

    its working

--- GameDev/Snake/SnakeGame.py
+++ GameDev/Snake/SnakeGame.py
@@ -1,6 +1,7 @@
 from Board import Board
 from Snake import Snake, Direction
 from time import sleep
+import keyboard
 import os
 
 class SnakeGame(object):
@@ -17,7 +18,18 @@ class SnakeGame(object):
     def __loop(self):
         while True:
             os.system('cls')
+            self.__getInput()
             self.__snake.move()
             self.__board.setSnakePos(self.__snake.getPosition())
             self.__board.showBoard()
             sleep(1/self.__snake.getSpeed())
+
+    def __getInput(self):
+        if keyboard.is_pressed('up'):
+            self.__snake.setDirection(Direction.UP)
+        elif keyboard.is_pressed('down'):
+            self.__snake.setDirection(Direction.DOWN)
+        elif keyboard.is_pressed('left'):
+            self.__snake.setDirection(Direction.LEFT)
+        elif keyboard.is_pressed('right'):
+            self.__snake.setDirection(Direction.RIGHT)
