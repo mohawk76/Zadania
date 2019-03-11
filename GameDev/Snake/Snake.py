31fdf97d8a590837bc752ed57c9142611c2e7032
local <mohawk76@wp.pl>
2019-03-11 09:14:01 +0100

    move

--- GameDev/Snake/Snake.py
+++ GameDev/Snake/Snake.py
@@ -11,16 +11,58 @@ class Snake(object):
         self.__length = 3
         self.__speed = 1
         self.__direction = Direction.RIGHT
-        self.__position = position
+
+        x=position[1]
+        y=position[0]
+
+        self.__body = [SnakeBody([y,x]), SnakeBody([y,x-1]), SnakeBody([y,x-2])]
+
 
     def getDirection(self):
         return self.__direction
+
+    def setDirection(self, direction):
+        self.__direction = direction
 
     def getLength(self):
         return self.__length
 
     def incLength(self):
         self.__length += 1
+        self.__body.append(SnakeBody(self.__body[-1].getPosition()))
 
     def incSpeed(self):
         self.__speed += 1
+
+    def move(self):
+        nextPos = self.__body[0]
+        for part in self.__body:
+            #głowa
+            if part == self.__body[0]:
+                coords = part.getPosition()
+
+                if self.__direction==Direction.UP:
+                    coords[0]-=1
+                elif self.__direction==Direction.DOWN:
+                    coords[0] += 1
+                elif self.__direction==Direction.LEFT:
+                    coords[1] += 1
+                elif self.__direction==Direction.RIGHT:
+                    coords[1] -= 1
+            #reszta elementów ciała
+            else:
+                temp = part.getPosition()
+                part.setPosition(nextPos)
+                nextPos = temp
+
+class SnakeBody(object):
+    def __init__(self, position):
+        self.__x = position[1]
+        self.__y = position[0]
+
+    def getPosition(self):
+        return [self.__y, self.__x]
+
+    def setPosition(self, position):
+        self.__x = position[1]
+        self.__y = position[0]
